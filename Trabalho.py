0d7c12fbd13b9baca7ae03454625f99fc01bf399
GabrielHVitusso <gab.vitusso@gmail.com>
2025-04-24 22:16:52 -0300

    Graficos Finais

--- Trabalho.py
+++ Trabalho.py
@@ -9,13 +9,13 @@ import matplotlib.pyplot as plt
 
 # Importacao dos dados da base de dados de vendas (Base 10) - Etapa 1 - Coleta de Dados
 
-Arquivo = '/home/gabriel/Documents/FolderProgamacao/Repositorio/GabrielHVitusso-1/Trabalho_Python_1/Dados/Base.xlsx'
+Arquivo = './Dados/Base.xlsx'
 
 # Leitura dos dados da décima página
 
 # Isso da a capacidade de se selecionar uma base de dados especifica
 Nome = "Base"
-i = 10
+i = 8
 i = str(i)
 Nome = Nome + i
 
@@ -56,22 +56,11 @@ Produtos = nomes[2:]
 Produtos.append("total")
 #print(Produtos)
 
-
+df['total'] = df[[df.columns[2], df.columns[3], df.columns[4], df.columns[5], df.columns[6], df.columns[7]]].sum(axis=1)
 # Inicializa a matriz com os valores calculados
 matriz_de_resultados = []
 
 for i in Produtos:
-    if i == "total":
-        # Exclui as duas primeiras colunas ao calcular os valores para "total"
-        df_i = df.iloc[:, 2:]
-        media = df_i.mean().mean()
-        mediana = df_i.median().median()
-        moda = stats.mode(df_i.values.flatten(), keepdims=True)
-        variancia = df_i.var().mean()
-        desvio_padrao = variancia ** 0.5
-        amplitude = df_i.max().max() - df_i.min().min()
-        matriz_de_resultados.append([i, media, mediana, moda.mode[0], variancia, desvio_padrao, amplitude])
-    else:
         media = df[i].mean()
         mediana = df[i].median()
         moda = stats.mode(df[i], keepdims=True)
@@ -114,3 +103,51 @@ while True:
         plt.show()
     else:
         print("Não existe essa correlação")
+        
+# Grafico de vendas de cada produto ao longo dos anos
+
+Produtos=nomes[2:] # Reinicia a linha de produtos
+
+plt.figure(figsize=(12, 6)) # Tamanho da figura
+for Produto in Produtos:
+    plt.plot(df['Mês'].astype(str) + '/' + df['Ano'].astype(str), df[Produto], label=Produto, marker='x') # Linha de qualquer produto
+plt.xlabel('Mês') # Nome do eixo x
+plt.ylabel('Vendas') # Nome do eixo y
+plt.title('Vendas de Produtos ao Longo do Tempo') # Nome do grafico
+plt.legend()    
+plt.grid(True) # Coloca uma grade no grafico
+plt.show()
+
+# Grafico de Pizza das proporções de vendas de cada produto
+
+plt.figure(figsize=(8, 8)) # Tamanho da figura
+Total_de_Vendas = []
+for Produto in Produtos:
+    Total_de_Vendas.append(df[Produto].sum()) # Soma as vendas de cada produto
+labels = Produtos # Nome dos produtos
+
+sizes = Total_de_Vendas # Tamanho de cada fatia
+plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90) # Cria o grafico de pizza
+plt.title("Proporções de Vendas de Produtos") # Nome do grafico
+plt.show()
+
+# Grafico de variancia dos 24 meses
+plt.figure(figsize=(8,6)) # Tamanho da figura
+plt.bar(df_resultados[df_resultados['Produto'] != 'total']['Produto'], df_resultados[df_resultados['Produto'] != 'total']['Variância']) # Cria o grafico de barras
+plt.xlabel('Produto') # Nome do eixo x
+plt.ylabel('Variância') # Nome do eixo y   
+plt.title('Variância dos Produtos') # Nome do grafico
+plt.xticks(rotation=90)
+plt.grid(True) # Coloca uma grade no grafico
+plt.show()
+
+# Grafico de Barra da moda dos produtos
+
+plt.figure(figsize=(8, 6)) # Tamanho da figura
+plt.bar(df_resultados[df_resultados['Produto'] != 'total']['Produto'], df_resultados[df_resultados['Produto'] != 'total']['Moda']) # Cria o grafico de barras
+plt.xlabel('Produto') # Nome do eixo x
+plt.ylabel('Moda') # Nome do eixo y   
+plt.title('Moda dos Produtos') # Nome do grafico
+plt.xticks(rotation=90)
+plt.grid(True) # Coloca uma grade no grafico
+plt.show()
